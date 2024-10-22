99610487d5def07bf28db83a9c00f3b546e62de1
Bono <bono.tadic@porsche.digital>
2024-10-22 02:10:14 +0200

    Update AuthConfig

--- src/main/kotlin/hr/fer/web2/ticketapp/AuthConfig.kt
+++ src/main/kotlin/hr/fer/web2/ticketapp/AuthConfig.kt
@@ -3,8 +3,10 @@ package hr.fer.web2.ticketapp
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher
 
@@ -14,29 +16,29 @@ class AuthConfig {
 
     @Bean
     @Order(1)
-    fun resourceServerFilter(http: HttpSecurity): SecurityFilterChain {
-        http
-            .securityMatcher(RequestHeaderRequestMatcher("Authorization"))
-            .authorizeHttpRequests { requests ->
-                requests.requestMatchers("/tickets").authenticated()
-            }
-            .oauth2ResourceServer { oauth2 ->
-                oauth2.jwt {}
+    fun resourceServerFilter(http: HttpSecurity) : SecurityFilterChain {
+        http {
+            securityMatcher(RequestHeaderRequestMatcher("Authorization"))
+            authorizeHttpRequests {
+                authorize(method = HttpMethod.POST, pattern = "/tickets/create", authenticated)
             }
 
+            oauth2ResourceServer {
+                jwt {  }
+            }
+        }
         return http.build()
     }
 
     @Bean
     @Order(2)
     fun loginFilter(http: HttpSecurity): SecurityFilterChain {
-        http
-            .authorizeHttpRequests { requests ->
-                requests
-                    .requestMatchers("/tickets/details/**").authenticated()
-                    .requestMatchers("/tickets/total").permitAll()
+        http {
+            authorizeHttpRequests {
+                authorize(method = HttpMethod.GET, pattern = "/tickets/details/**", authenticated)
             }
-            .oauth2Login {}
+            oauth2Login {  }
+        }
 
         return http.build()
     }
